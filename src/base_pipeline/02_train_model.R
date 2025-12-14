62381a5d1b42e693bd7ca526501326e40ee00e93
mdrummon <mdrummon@buffalo.edu>
2025-12-13 21:46:46 -0500

    added comments

--- src/base_pipeline/02_train_model.R
+++ src/base_pipeline/02_train_model.R
@@ -59,8 +59,22 @@ k <- 5
 #set seed to ensure reproducibility
 set.seed(123)
 
+#create a list of row indicies for each fold
+#seq_len(nrow(train_matrix)) - sequence of row indicies from 1 to number of training samples
+#rep(1:k, length.out = nrow(train_matrix)) - repeats fold numbers 1-k until all rows are assigned
+#sample(...) - randomly shuffle fold assignments
+#split(..., ...) - separates row indices into k groups, stored in a list
+#where each element corresponds to one fold
 cv_folds <- split(seq_len(nrow(dtrain)), sample(rep(1:k, length.out = nrow(dtrain))))
 
+#call to the xgboost cross validation function
+#params = params, - use the previously defined hyperparameters
+#data = dtrain - specify the training data in DMatrix format
+#nrounds = 2000 - max number of trees to train
+#folds = cv_folds - manual list of fold indices for cross validation
+#early_stopping_rounds = 50 - stop training if the evaluation metric does not improve for 50 consecutive rounds
+#maximize = TRUE - tell XGBoost to maximize the evaluation metric
+#verbose = 1 - tell xgboost to print training progress to console
 cv_model <- xgb.cv(
   params = params,
   data = dtrain,
@@ -71,12 +85,14 @@ cv_model <- xgb.cv(
   verbose = 1
 )
 
+#retrieve the optimal number of boosting rounds from the cross validation results
+#used to train the final model with the best number of trees
 best_nrounds <- cv_model$best_iteration
 
 # Train the model with xgb.train()
 # params - the model parameters needed
 # data - the training data, an xgb.DMatrix
-# nrounds - max number of iterations (trees) to build the model
+#nrounds = best_nrounds - train for the optimal number of boosting rounds from CV
 # watchlist - which datasets to monitor during training. observe model perf in real time
 # early_stopping_round - if validation AUC has not improved for 30 rounds, stop training
 # print_every_n - training progress printed to console every n rounds
@@ -90,9 +106,7 @@ xgb_model <- xgb.train(
 )
 
 # Save the trained model to the created directory
-
-#dir.create("models", showWarnings = FALSE)
-#xgb.save(xgb_model, "models/xgb_fraud.model")
+xgb.save(xgb_model, "models/xgb_fraud.model")
 
 #print feature importance matrix, which features were most useful for making predictions
 importance_matrix <- xgb.importance(model = xgb_model)
